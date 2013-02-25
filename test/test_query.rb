1980b0c0138dfeab2ad914fab5617ca2fb13437f
Marc Hébert <crambert@gmail.com>
2013-02-25 01:09:59 -0500

    #8: parse Query test

--- test/test_query.rb
+++ test/test_query.rb
@@ -5,11 +5,23 @@ module Test
   class TestQuery < Test::Unit::TestCase
     def test_set_from_config
       query = Ghiv::Query.new
-      queries = {'query_creator' => 'marcheb', 'query_direction' => 'asc', 'query_labels' => 'label1,label2', 'query_milestone' => '2', 'query_sort' => 'created', 'query_state' => 'open'}
-      queries.each { |k,v| Ghiv::Config.send("#{k}=",v) }
+
+      queries.each { |k,v| Ghiv::Config.send("query_#{k}=",v) }
       query.set_from_config
 
-      queries.each { |k,v| assert_equal(query.send(k.gsub('query_','')), v) }
+      queries.each { |k,v| assert_equal(v, query.send(k)) }
+    end
+
+    def test_parse
+      query = Ghiv::Query.new
+      queries.each { |k,v| query.send(k,v) }
+
+      query.parse
+      queries.each { |k,v| assert query.elements.include?("#{k}=#{v.is_a?(Array) ? v.join(',') : v}") }
+    end
+
+    def queries
+      {'creator' => 'marcheb', 'direction' => 'asc', 'labels' => ['label1', 'label2'], 'milestone' => '2', 'sort' => 'created', 'state' => 'open'}
     end
   end
 end
