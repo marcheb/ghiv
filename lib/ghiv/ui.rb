03cd5f994e4a38f8e7aec4f6e358cb7b89252f75
Marc Hébert <crambert@gmail.com>
2013-02-28 21:03:28 -0500

    close #12 - mushroom

--- lib/ghiv/ui.rb
+++ lib/ghiv/ui.rb
@@ -13,12 +13,35 @@ module Ghiv
         puts "issue number: #{issue.number}"
         puts "title: #{issue.title}"
         puts "body:"
-        puts issue.body if not issue.body.empty?
+        puts Emoji.parser(issue.body) if not issue.body.empty?
         puts "labels: #{ issue.labels.map{ |l| l['name'] }.join(',') }"
         puts "created_at: #{issue.created_at}"
         puts "state: #{issue.state}"
         puts "html_url : #{issue.html_url}"
         puts "url : #{issue.url}"
+      end
+
+      module Emoji
+        def self.parser(text)
+          text.gsub!(':mushroom:', mushroom)
+        end
+
+        def self.mushroom
+          <<-eos
+               n
+              / `\\
+             (___:)
+              """"
+               ||
+               ||
+               ))
+              //
+             ((
+              \\\\\\
+               ))
+               ||
+          eos
+        end
       end
     end
   end
